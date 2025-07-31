15046a4a8940f94a0b8096a9ddb8a719933af4e8
Mustafa Yalçınkaya <myalcinkaya136@gmail.com>
2025-08-01 01:10:07 +0300

    commit

--- BankApi.DataAccess/obj/Debug/net8.0/BankApi.DataAccess.AssemblyInfo.cs
+++ BankApi.DataAccess/obj/Debug/net8.0/BankApi.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankApi.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6af079dea3c9bc936737eec056f3e502621c7b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankApi.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankApi.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
