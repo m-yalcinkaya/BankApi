fc1d53c8b8e44c11f2aa99e617bf65e1180cf25a
Mustafa Yalçınkaya <myalcinkaya136@gmail.com>
2025-07-31 17:41:08 +0300

    update app.settings.json

--- BankApi/obj/Debug/net8.0/BankApi.AssemblyInfo.cs
+++ BankApi/obj/Debug/net8.0/BankApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83e8076703070cc05b070616dc4c4fa0f4b6dda6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
